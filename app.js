ee5fc9878e74361e8f80de322d3083b6a2723018
affu7272 <afzalnomani1@gmail.com>
2024-06-18 01:21:31 +0530

    added env variables

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ import morgan from 'morgan';
 import tourRouter from './routes/tourRoutes.js';
 import  userRouter from './routes/userRoutes.js';
 
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 const app = express();
 
 //1) MIDDLEWARES
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+} 
+
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Middleware');
-  next();
-});
+app.use(express.static('./public'))
 
 //Mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 export default app;
+
