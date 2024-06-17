4e12a16c9cb578fa3a996b504fdfe6d0528ad778
affu7272 <afzalnomani1@gmail.com>
2024-06-18 01:22:08 +0530

    added env variable

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 
 import app from "./app.js";
+// console.log(process.env.NODE_ENV);
 // 4) START SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
+
