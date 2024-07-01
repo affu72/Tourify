7d12574c7c327a495bcdc376a7b5b8bb297b26f5
affu7272 <afzalnomani1@gmail.com>
2024-07-01 09:15:41 +0530

    project linting

--- app-v2.js
+++ app-v2.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log("Middleware");
+  // console.log("Middleware");
   next();
 })
 
--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 import express from 'express';
 import morgan from 'morgan';
-import tourRouter from './routes/tourRoutes.js';
-import  userRouter from './routes/userRoutes.js';
-
 import dotenv from 'dotenv';
+import tourRouter from './routes/tourRoutes';
+import userRouter from './routes/userRoutes';
+
 dotenv.config({ path: '.env' });
 
 const app = express();
 
 //1) MIDDLEWARES
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-} 
+}
 
 app.use(express.json());
 
-app.use(express.static('./public'))
+app.use(express.static('./public'));
 
 //Mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 export default app;
-
